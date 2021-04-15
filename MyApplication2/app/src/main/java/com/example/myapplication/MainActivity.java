487cb49d8f19da63659b26eba485d4160997fdcd
Mirella <mirella.lds@gmail.com>
2021-04-14 21:29:01 -0300

    Create scren, with toast message and count textview

--- MyApplication2/app/src/main/java/com/example/myapplication/MainActivity.java
+++ MyApplication2/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -3,12 +3,29 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private int valorDaSoma = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void soma(View view) {
+        valorDaSoma += 1;
+
+        TextView componenteDeTexto = findViewById(R.id.textView);
+
+        componenteDeTexto.setText(String.valueOf(valorDaSoma));
+    }
+
+    public void mostrarMensagem(View botao1) {
+        Toast.makeText(this, "Mensagem do botão 1", Toast.LENGTH_LONG).show();
     }
 }
